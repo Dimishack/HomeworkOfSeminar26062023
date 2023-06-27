933a294fdf1bef8f9c7e57d520e992251b544e8e
Dimishack <dimishack.git@mail.ru>
2023-06-27 16:49:24 +0300

    Добавление журнала в Target15 =)

--- Target15.cs
+++ Target15.cs
@@ -12,19 +12,28 @@ namespace HomeworkOfSeminar26062023
     {
         internal static void DayOfWeek()
         {
-            string[] dayOfWeeks = { "Понедельник", "Вторник", "Среда", "Четверг", 
+            ConsoleKey key;
+            do
+            {
+                string[] dayOfWeeks = { "Понедельник", "Вторник", "Среда", "Четверг",
                                     "Пятница", "Суббота", "Воскресенье" };
 
-            Console.Write("Введите цифру, обозначающую день недели (от 1 до 7): ");
-            string digitForDayOFWeek = Console.ReadLine();
-            if (digitForDayOFWeek.Length == 1 && digitForDayOFWeek != "0"
-                && sbyte.TryParse(digitForDayOFWeek, out sbyte digit))
-            {
-                Console.WriteLine("Цифра {0} является днем недели: {1}", digit, dayOfWeeks[digit - 1]);
-                Console.WriteLine((digit > 5) ? "И он является выходным днем" : "И он не является выходным днем");
-            }
-            else
-                Console.WriteLine("Данное значение не является цифрой, обозначающей день недели!");
+                Console.Write("Введите цифру, обозначающую день недели (от 1 до 7): ");
+                string digitForDayOFWeek = Console.ReadLine();
+                if (digitForDayOFWeek.Length == 1 && digitForDayOFWeek != "0"
+                    && sbyte.TryParse(digitForDayOFWeek, out sbyte digit))
+                {
+                    Console.WriteLine("Цифра {0} является днем недели: {1}", digit, dayOfWeeks[digit - 1]);
+                    Console.WriteLine((digit > 5) ? "И он является выходным днем" : "И он не является выходным днем");
+                }
+                else
+                    Console.WriteLine("Данное значение не является цифрой, обозначающей день недели!");
+
+                Console.WriteLine("\nEnter - Вернуться к журналу \nЛюбая другая клавиша - решить заново");
+                Console.Write("Выберите действие: ");
+                key = Console.ReadKey().Key;
+                Console.WriteLine();
+            } while (key != ConsoleKey.Enter);
         }
     }
 }
