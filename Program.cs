8acbfd58b325c85ba788051cebe4910383dc75a2
Dimishack <dimishack.git@mail.ru>
2023-06-27 12:47:41 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HomeworkOfSeminar26062023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
